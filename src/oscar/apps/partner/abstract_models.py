d7ddc96024acded98118dbdf41cb351017d4076b
Arthur Case <arthur@studio-automatique.com>
2016-10-04 12:49:24 +0800

    Remove race conditions for stock records
    Allocating, consuming and cancelling stocks are handled in thread safe update queries.
    
    See Issue #1811.

--- src/oscar/apps/partner/abstract_models.py
+++ src/oscar/apps/partner/abstract_models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Case, F, When
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
@@ -186,10 +187,14 @@ class AbstractStockRecord(models.Model):
         This normally happens when a product is bought at checkout.  When the
         product is actually shipped, then we 'consume' the allocation.
         """
-        if self.num_allocated is None:
-            self.num_allocated = 0
-        self.num_allocated += quantity
-        self.save()
+
+        self.__class__.objects.select_for_update().filter(pk=self.pk).update(
+            num_allocated=Case(
+                When(num_allocated__isnull=True,
+                     then=(0 + quantity)),
+                default=F('num_allocated') + quantity
+            ),
+        )
     allocate.alters_data = True
 
     def is_allocation_consumption_possible(self, quantity):
@@ -208,16 +213,18 @@ class AbstractStockRecord(models.Model):
         if not self.is_allocation_consumption_possible(quantity):
             raise InvalidStockAdjustment(
                 _('Invalid stock consumption request'))
-        self.num_allocated -= quantity
-        self.num_in_stock -= quantity
-        self.save()
+        self.__class__.objects.filter(pk=self.pk).update(
+            num_allocated=F('num_allocated') - quantity,
+            num_in_stock=F('num_in_stock') - quantity,
+        )
     consume_allocation.alters_data = True
 
     def cancel_allocation(self, quantity):
         # We ignore requests that request a cancellation of more than the
         # amount already allocated.
-        self.num_allocated -= min(self.num_allocated, quantity)
-        self.save()
+        self.__class__.objects.filter(pk=self.pk).update(
+            num_allocated=F('num_allocated') - min(self.num_allocated, quantity),
+        )
     cancel_allocation.alters_data = True
 
     @property
